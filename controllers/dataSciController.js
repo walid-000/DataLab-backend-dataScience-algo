27ca9b427acc66ea5c4eb6aed8c3e5668b8f4111
walid <walid.shailkh.3810461@ves.ac.in>
2025-01-07 15:32:12 +0530

    intercept , slope , rsq fixed

--- controllers/dataSciController.js
+++ controllers/dataSciController.js
@@ -1,15 +1,20 @@
 const ss = require("simple-statistics");
-
+const calculateRSquared = require("./calculateRsq")
 function handleSimpleLinearRegression(req , res){
     const {data : {xField , yField}}  = req.body ;
     const {data } = req.body ;
-    const LinearRegression = ss.linearRegression([xField , yField])
-    console.log(data);
-    console.log(xField)
-    console.log(yField)
+    const processdata = xField.map((item , index)=> [item , yField[index]]);
+    const LinearRegression = ss.linearRegression(processdata)
+    console.log("x " ,xField)
+    console.log("y " ,yField)
+    console.log("process data" , processdata)
     const slope =  LinearRegression.m ;
     const intercept =    LinearRegression.b ;
-    return res.status(200).json({"slope" : slope , "intercept" : intercept});
+    const yPredict = xField.map((x) => slope*x + intercept);
+    console.log("y predict  : " , yPredict)
+    const rSq = calculateRSquared(yField ,yPredict)
+    console.log(`slope ${slope} intercept ${intercept}  and for value of x 1 y => ${slope*1 + intercept} score is ${rSq}`);
+    return res.status(200).json({"slope" : slope , "intercept" : intercept , "rSq" : rSq});
 }
 
 module.exports = {
